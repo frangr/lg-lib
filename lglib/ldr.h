f47a156a21280e63f4dce3cc5632b6a6b2a065b1
fr::gr <frrgrr9@gmail.com>
2017-06-06 21:38:36 +0200

    Update ldr.h

--- lglib/ldr.h
+++ lglib/ldr.h
@@ -1,12 +1,12 @@
 #ifndef LDR_H_INCLUDED
 #define LDR_H_INCLUDED
 
-#include "and.h"
-#include "nand.h"
-#include "or.h"
-#include "nor.h"
-#include "ldrf.h"
-#include "negand.h"
-#include "negor.h"
+#include "lg/and.h"
+#include "lg/nand.h"
+#include "lg/or.h"
+#include "lg/nor.h"
+#include "lg/ldrf.h"
+#include "lg/negand.h"
+#include "lg/negor.h"
 
 #endif // LDR_H_INCLUDED
