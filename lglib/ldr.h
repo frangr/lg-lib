867f88506a9e9f67f1bf11a1df5dca0d45853a66
fr::gr <frrgrr9@gmail.com>
2017-06-07 19:31:54 +0200

    Add files via upload

--- lglib/ldr.h
+++ lglib/ldr.h
@@ -1,12 +1,14 @@
 #ifndef LDR_H_INCLUDED
 #define LDR_H_INCLUDED
 
-#include "lg/and.h"
-#include "lg/nand.h"
-#include "lg/or.h"
-#include "lg/nor.h"
-#include "lg/ldrf.h"
-#include "lg/negand.h"
-#include "lg/negor.h"
+#include "ldrf.h"
+#include "and.h"
+#include "nand.h"
+#include "or.h"
+#include "nor.h"
+#include "negand.h"
+#include "negor.h"
+#include "xor.h"
+#include "xnor.h"
 
 #endif // LDR_H_INCLUDED
