e97eddc6341747b036c29aab4221c9da21469e72
fr::gr <frrgrr9@gmail.com>
2017-06-18 18:08:21 +0200

    Add files via upload

--- lglib/lg/ldrf.h
+++ lglib/lg/ldrf.h
@@ -1,11 +1,33 @@
 #ifndef LDRF_H_INCLUDED
 #define LDRF_H_INCLUDED
 
+#include<iostream>
+
 namespace ldr
 {
+    using byte = unsigned char;
     using bit = bool;
 
     bit NOT(bit bt);
+
+
+    template<typename... T>
+    int btn(T&&... n);
+}
+
+template<typename... T>
+int ldr::btn(T&&... n)
+{
+    int s = sizeof...(T), pn = 1, bn = 0;
+    bit vc[s] = {std::forward<bit>(n)...};
+
+    while(s--)
+    {
+        if(vc[s])
+            bn += pn;
+        pn += pn;
+    }
+    return bn;
 }
 
 #endif // LDRF_H_INCLUDED
