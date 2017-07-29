4866b87f3aee28ae83e1cc612c2373354131da97
fr::gr <frrgrr9@gmail.com>
2017-07-30 00:19:03 +0200

    Update ldr.h

--- lglib/ldr.h
+++ lglib/ldr.h
@@ -1,16 +1,16 @@
 #ifndef LDR_H_INCLUDED
 #define LDR_H_INCLUDED
 
-#include "ldrf.h"
-#include "and.h"
-#include "nand.h"
-#include "or.h"
-#include "nor.h"
-#include "negand.h"
-#include "negor.h"
-#include "xor.h"
-#include "xnor.h"
-#include "mem.h"
-#include "alu.h"
+#include "lg/ldrf.h"
+#include "lg/and.h"
+#include "lg/nand.h"
+#include "lg/or.h"
+#include "lg/nor.h"
+#include "lg/negand.h"
+#include "lg/negor.h"
+#include "lg/xor.h"
+#include "lg/xnor.h"
+#include "lg/mem.h"
+#include "lg/alu.h"
 
 #endif // LDR_H_INCLUDED
